2bb5ffb65b7094376ee95ca553bdd21c65b8578e
Josh King <skierx@gmail.com>
2018-12-17 23:47:19 -0500

    Client side support to provide user feedback.
    * Adds a new coffeescript override for the pollCommonActionPanelDirective which wraps all of the various poll types. Only the userCanParticpate function gets overridden (the rest of the directive is unmodified).
    
    * Adds a translation that updates the text of the existing error message instead of modifying the HTML.

--- plugin.rb
+++ plugin.rb
@@ -4,11 +4,17 @@ module Plugins
             setup! :new_member_waiting_period do |plugin|
                 plugin.enabled = true
 
+                # This ability adds the server-side restriction
                 plugin.use_class 'models/ability/new_member_waiting_period'
                 plugin.extend_class Ability::Base do
                     prepend Ability::NewMemberWaitingPeriod
                 end
 
+                # This asset adds the client-side restriction
+                plugin.use_asset 'assets/new_member_waiting_period.coffee'
+
+                # This updates the wording of the client-side error message
+                plugin.use_translations("config/locales", :new_member_waiting_period)
             end
         end
     end
